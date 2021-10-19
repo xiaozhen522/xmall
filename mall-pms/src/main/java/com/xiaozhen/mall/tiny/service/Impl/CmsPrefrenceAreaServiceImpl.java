372b64be7c78abc9f1def0cbc0036148f5810a9f
xiaozhen522 <65516486+xiaozhen522@users.noreply.github.com>
2021-10-19 21:40:32 +0800

    商品优选管理接口完成

---
+++ mall-pms/src/main/java/com/xiaozhen/mall/tiny/service/Impl/CmsPrefrenceAreaServiceImpl.java
@@ -0,0 +1,26 @@
+package com.xiaozhen.mall.tiny.service.Impl;
+
+import com.xiaozhen.mall.tiny.mbg.mapper.CmsPrefrenceAreaMapper;
+import com.xiaozhen.mall.tiny.mbg.model.CmsPrefrenceArea;
+import com.xiaozhen.mall.tiny.mbg.model.CmsPrefrenceAreaExample;
+import com.xiaozhen.mall.tiny.service.CmsPrefrenceAreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @description: 商品优选CmsPrefrenceAreaService实现类
+ * @create time:21:34
+ * @Author: XiaoZhen
+ **/
+@Service
+public class CmsPrefrenceAreaServiceImpl implements CmsPrefrenceAreaService {
+    @Autowired
+    private CmsPrefrenceAreaMapper prefrenceAreaMapper;
+
+    @Override
+    public List<CmsPrefrenceArea> listAllPrefrenceArea() {
+        return prefrenceAreaMapper.selectByExample(new CmsPrefrenceAreaExample());
+    }
+}
