26228be172f6c72f70cf7cff4a7810ac0becceea
xiaozhen522 <65516486+xiaozhen522@users.noreply.github.com>
2021-10-25 14:32:35 +0800

    首页专题推荐管理接口完成

---
+++ mall-pms/src/main/java/com/xiaozhen/mall/tiny/service/SmsHomeRecommendSubjectService.java
@@ -0,0 +1,24 @@
+package com.xiaozhen.mall.tiny.service;
+
+import com.xiaozhen.mall.tiny.mbg.model.SmsHomeRecommendSubject;
+
+import java.util.List;
+
+/**
+ * @description: 首页专题推荐
+ * @create time:14:05
+ * @Author: XiaoZhen
+ **/
+public interface SmsHomeRecommendSubjectService {
+
+    int createHomeRecommendSubject(SmsHomeRecommendSubject[] homeRecommendSubjectList);
+
+    int deleteHomeRecommendSubject(Long[] ids);
+
+    List<SmsHomeRecommendSubject> listHomeRecommendSubject(Integer pageNum, Integer pageSize, Integer recommendStatus,
+                                                           String subjectName);
+
+    int updateRecommendStatus(Long[] ids, Integer recommendStatus);
+
+    int updateSort(Long id, Integer sort);
+}
