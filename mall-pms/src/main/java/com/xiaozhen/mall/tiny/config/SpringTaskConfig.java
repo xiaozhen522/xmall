ba42cab649c90920b9285b217aab9d458256f498
xiaozhen522 <65516486+xiaozhen522@users.noreply.github.com>
2021-10-26 20:10:53 +0800

    通用定时任务完成

---
+++ mall-pms/src/main/java/com/xiaozhen/mall/tiny/config/SpringTaskConfig.java
@@ -0,0 +1,14 @@
+package com.xiaozhen.mall.tiny.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @description : 定时任务配置
+ * @create time:2021/10/26
+ * @Author : XiaoZhen
+ **/
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
